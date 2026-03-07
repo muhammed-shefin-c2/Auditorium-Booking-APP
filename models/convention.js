77884d8759a47f944f54a35b1a08eb0c764b3186
muhammed-shefin-c2 <shefinc2@gmail.com>
2026-03-07 20:14:20 +0530

    Add GST number field with validation to convention model

--- models/convention.js
+++ models/convention.js
@@ -177,6 +177,16 @@ const convention_schema = new mongoose.Schema({
     required: true,
     type: String
   },
+  gst_number: {
+    type: String,
+    required: true,
+    unique: true,
+    uppercase: true,
+    match: [
+      /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/,
+      "Please enter a valid GST number"
+    ]
+  },
   bank: bank_schema,
   rating: [rating_schema],
   bookings: [booking_schema]
